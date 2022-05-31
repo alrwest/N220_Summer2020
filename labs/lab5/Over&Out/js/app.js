aa290414a50344a03d2b254963a17b5802d32501
alrwest <alrwest@iu.edu>
2022-05-31 03:56:13 -0400

    updating over and out

--- labs/lab5/Over&Out/js/app.js
+++ labs/lab5/Over&Out/js/app.js
@@ -1,1 +1,16 @@
 //Over & Out
+
+//hover off = blue
+//hover on = black
+
+//created new variable pulled from id = square to pass into parameters of each function
+var newSquare = document.getElementById("square");
+
+//creating a function passing newSquare as parameter; creates hover on effect where hovering cursor changes square black with onmouseover
+function hoverOn(newSquare) {
+   newSquare = document.getElementById("square").style.backgroundColor = "#000000";
+}
+//creating a function passing newSquare as parameter; returns square as blue with onmouseout
+function hoverAway(newSquare) {
+    newSquare = document.getElementById("square").style.backgroundColor = "#0e1478";
+}
